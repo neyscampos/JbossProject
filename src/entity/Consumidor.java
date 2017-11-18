c8677a4c8417862e1993067701c7d0ef79157246
Joao Santana <wozefacu@cartelera.org>
2017-11-18 11:12:31 -0200

    sequencia no projeto

--- src/entity/Consumidor.java
+++ src/entity/Consumidor.java
@@ -1,19 +1,23 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
 @Table(name = "consumidor")
-@NamedQuery(name="mostra",query="select c from Consumidor as c")
+@NamedQuery(name = "mostra", query = "select c from Consumidor as c")
 public class Consumidor implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -21,46 +25,95 @@ public class Consumidor implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@Column(length=100)
+	@Column(length = 100)
 	private String nome;
-	@Column(length=100, unique=true)
+	@Column(length = 100, unique = true)
 	private String email;
-	@Column(length=25, unique=true)
+	@Column(length = 25, unique = true)
 	private String telefone;
 	@Transient
 	private Double total = 0.;
-	
+
+	@OneToMany(mappedBy = "consumidor", fetch = FetchType.EAGER)
+	private Collection<Consumo> consumos;
+
+	public Collection<Consumo> getConsumos() {
+		return consumos;
+	}
+
+	public void adicionar(Consumo... consumo) {
+		for (Consumo c : consumo) {
+			consumos.add(c);
+		}
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
 	public Double getTotal() {
 		return total;
 	}
+
 	public void setTotal(Double total) {
 		this.total = total;
 	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
+	public Consumidor() {
+		this.consumos = new ArrayList<Consumo>();
+	}
+
+	public Consumidor(Integer id, String nome, String email, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.consumos = new ArrayList<Consumo>();
+	}
+
+	public void calcularSubtotal() {
+		this.total = 0.;
+		for (Consumo c : consumos) {
+			c.setSubtotal(c.getPreco() * c.getQuantidade());
+			this.total += c.getSubtotal();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Consumidor [nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", total=" + total + "]";
+	}
+
 }
---
+++ src/entity/Consumo.java
@@ -0,0 +1,113 @@
+package entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "consumo")
+public class Consumo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idConsumo;
+	@Column(length = 50)
+	private String nomeProduto;
+	@Column
+	private Double preco;
+	@Column
+	private Integer quantidade;
+	@Transient
+	private Double subtotal = 0.;
+	@ManyToOne
+	@JoinColumn(name = "fkidConsumidor")
+	private Consumidor consumidor;
+
+	public Consumo() {
+		super();
+	}
+
+	public Consumo(Integer idConsumo, String nomeProduto, Double preco, Integer quantidade) {
+		super();
+		this.idConsumo = idConsumo;
+		this.nomeProduto = nomeProduto;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.subtotal = subtotal;
+		this.consumidor = consumidor;
+	}
+
+	@Override
+	public String toString() {
+		return "Consumo [idConsumo=" + idConsumo + ", nomeProduto=" + nomeProduto + ", preco=" + preco + ", quantidade="
+				+ quantidade + ", subtotal=" + subtotal + "]";
+	}
+
+	public Integer getIdConsumo() {
+		return idConsumo;
+	}
+
+	public void setIdConsumo(Integer idConsumo) {
+		this.idConsumo = idConsumo;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public static void main(String[] args) {
+		Consumidor c = new Consumidor(10, "firmino", "firmino@gmail.com", "3333");
+		Consumo c1 = new Consumo(101, "refrigerante", 7., 8);
+		Consumo c2 = new Consumo(102, "redbull", 14., 3);
+		Consumo c3 = new Consumo(103, "pizza maracana", 55., 5);
+
+		c.adicionar(c1, c2, c3);
+		c.calcularSubtotal();
+
+		System.out.println(c);
+		System.out.println(c.getConsumos());
+
+	}
+
+}
